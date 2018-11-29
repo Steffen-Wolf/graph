6d8db24be7599fe70eaa077c227b6e93171e628c
Steffen_Wolf <Steffen.Wolf@stud.uni-heidelberg.de>
2018-11-29 21:13:03 +0100

    fix KL label export

--- src/python/lib/multicut/kernighan-lin.cxx
+++ src/python/lib/multicut/kernighan-lin.cxx
@@ -15,24 +15,25 @@ namespace multicut {
         module.def("kernighan_lin", [](const Graph<> & graph,
                                          const xt::pytensor<double, 1> & edge_values,
                                          const float epsilon) {
-            const KernighanLinSettings setting;
-            // setting.epsilon = epsilon;
+            KernighanLinSettings setting;
+            setting.epsilon = epsilon;
 
             // TODO hacked in node labels for convinience
-            // const int64_t n_edges = graph.numberOfEdges();
             const int64_t n_edges = graph.numberOfVertices();
-            // xt::pytensor<char, 1> edge_labels = xt::zeros<char>({n_edges});
-            //xt::pytensor<uint32_t, 1> edge_labels = xt::zeros<char>({n_edges});
+            
+            // warmstart KL with one cluster per node
             std::vector<uint64_t> edge_labels = std::vector<uint64_t>(n_edges);
+            int k = 0;
+            std::fill(edge_labels.begin(), edge_labels.end(), k++);
+
+            xt::pytensor<uint64_t, 1> edge_labels_exp = xt::zeros<char>({n_edges});
             py::gil_scoped_release allow_threads;
             {
-                kernighanLin(graph, edge_values, edge_labels, setting);
-            }
-
-            // convert std::vector edge_labels to xtensor array for export
-            xt::pytensor<uint64_t, 1> edge_labels_exp = xt::zeros<char>({n_edges});
-            for (int i = 0; i < n_edges; ++i){
-                edge_labels_exp(i) = edge_labels[i];
+                std::vector<uint64_t> tmp_labels = kernighanLin(graph, edge_values, edge_labels, setting);
+                // convert std::vector edge_labels to xtensor array for export
+                for (int i = 0; i < n_edges; ++i){
+                    edge_labels_exp(i) = tmp_labels[i];
+                }
             }
 
             return edge_labels_exp;
